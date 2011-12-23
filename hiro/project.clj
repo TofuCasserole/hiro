8b2a4b2977a85ae488c51891df548e62e7792a4f
Kristoffe Zehr <kristoffe.zehr@gmail.com>
2011-12-23 03:51:06 -0600

    added initial clojure files, framenet data, and pdfs

---
+++ hiro/project.clj
@@ -0,0 +1,3 @@
+(defproject hiro "0.1.0-SNAPSHOT"
+  :description "An interactive storytelling engine written in clojure."
+  :dependencies [[org.clojure/clojure "1.3.0"]])
---
+++ hiro/test/hiro/test/core.clj
@@ -0,0 +1,6 @@
+(ns hiro.test.core
+  (:use [hiro.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
